da8b885f8459e0471489d35a624595dfed124dec
Gerald Celanji <gerald.celanji@outlook.com>
2022-05-05 09:45:52 +0100

    Added MemesApiService

---
+++ app/src/main/java/com/example/memescollection/model/remote/MemesApiService.kt
@@ -0,0 +1,13 @@
+package com.example.memescollection.model.remote
+
+import com.example.memescollection.common.END_POINT
+import com.example.memescollection.model.MemesResponse
+import retrofit2.Response
+import retrofit2.http.GET
+
+
+interface MemesApiService {
+
+    @GET(END_POINT)
+    suspend fun getMemes() : Response<MemesResponse>
+}
