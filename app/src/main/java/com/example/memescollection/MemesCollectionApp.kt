fcae7d0f84a06c459134d08a264b271af7870ce6
Gerald Celanji <gerald.celanji@outlook.com>
2022-05-05 11:22:16 +0100

    Added Hilt Application and Network Module

---
+++ app/src/main/java/com/example/memescollection/MemesCollectionApp.kt
@@ -0,0 +1,7 @@
+package com.example.memescollection
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MemesCollectionApp : Application()
---
+++ app/src/main/java/com/example/memescollection/di/NetworkModule.kt
@@ -0,0 +1,44 @@
+package com.example.memescollection.di
+
+import com.example.memescollection.common.BASE_URL
+import com.example.memescollection.model.remote.MemesApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+@Module
+@InstallIn
+class NetworkModule {
+
+    @Provides
+    fun providesHttpLoggingInterceptor(): HttpLoggingInterceptor =
+        HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+    @Provides
+    fun provideOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    fun provideMemesApiService(okHttpClient: OkHttpClient): MemesApiService {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+            .create(MemesApiService::class.java)
+    }
+
+}
