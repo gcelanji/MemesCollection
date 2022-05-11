ce2b4d0bad75e702289819273ff96ce57d50f295
Gerald Celanji <gerald.celanji@outlook.com>
2022-05-11 12:18:20 +0100

    1.2.1 - Implemented save to storage feature

--- app/src/main/java/com/example/memescollection/view/adapters/MemesAdapter.kt
+++ app/src/main/java/com/example/memescollection/view/adapters/MemesAdapter.kt
@@ -1,5 +1,7 @@
 package com.example.memescollection.view.adapters
 
+import android.icu.number.NumberFormatter.with
+import android.icu.number.NumberRangeFormatter.with
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -7,15 +9,20 @@ import com.example.memescollection.databinding.MemeItemLayoutBinding
 import com.example.memescollection.model.Meme
 import com.squareup.picasso.Picasso
 
-class MemesAdapter(private val data: List<Meme>) :
+class MemesAdapter(private val data: List<Meme>, private val downloadImage: (Meme) -> Unit) :
     RecyclerView.Adapter<MemesAdapter.MemesViewHolder>() {
 
     class MemesViewHolder(private val binding: MemeItemLayoutBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        fun onBind(dataItem: Meme) {
+        fun onBind(dataItem: Meme, downloadImage: (Meme) -> Unit) {
             Picasso.get().load(dataItem.url).into(binding.ivMemeImage)
             binding.tvMemeTitle.text = dataItem.name
+
+            binding.acbSave.setOnClickListener{
+                downloadImage(dataItem)
+            }
+
         }
     }
 
@@ -33,7 +40,7 @@ class MemesAdapter(private val data: List<Meme>) :
     }
 
     override fun onBindViewHolder(holder: MemesAdapter.MemesViewHolder, position: Int) {
-        return holder.onBind(data[position])
+        return holder.onBind(data[position], downloadImage)
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/example/memescollection/view/fragments/MemesListFragment.kt
+++ app/src/main/java/com/example/memescollection/view/fragments/MemesListFragment.kt
@@ -1,13 +1,18 @@
 package com.example.memescollection.view.fragments
 
+import android.R
+import android.app.DownloadManager
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -16,7 +21,10 @@ import com.example.memescollection.model.Meme
 import com.example.memescollection.model.UIState
 import com.example.memescollection.view.adapters.MemesAdapter
 import com.example.memescollection.viewmodel.MemesViewModel
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+import java.io.File
+
 
 private const val TAG = "MemesListFragment"
 
@@ -24,7 +32,7 @@ private const val TAG = "MemesListFragment"
 class MemesListFragment : Fragment() {
 
     private lateinit var binding: MemeItemsListLayoutBinding
-    private lateinit var progressBar : ProgressBar
+    private lateinit var snackbar: Snackbar
     private val viewModel: MemesViewModel by lazy {
         ViewModelProvider(this)[MemesViewModel::class.java]
     }
@@ -54,7 +62,7 @@ class MemesListFragment : Fragment() {
                 is UIState.ResponseMemesList -> {
                     Log.d(TAG, "initObservables: $response")
                     // bind data to the view
-                    // Just to show the Progress Bar :P
+                    // Delay 3 seconds just to show the Progress Bar :P
                     Handler(Looper.getMainLooper()).postDelayed({
                         updateUI(responseData = response.data.data.memes)
                     }, 3000)
@@ -75,10 +83,50 @@ class MemesListFragment : Fragment() {
     }
 
     private fun updateUI(responseData: List<Meme>) {
-        adapter = MemesAdapter(data = responseData)
+        adapter = MemesAdapter(data = responseData) {
+            download(it)
+        }
         binding.rvMemesList.layoutManager = LinearLayoutManager(context)
         binding.rvMemesList.adapter = adapter
         binding.progressbar.visibility = View.INVISIBLE
     }
+
+    private fun download(item: Meme) {
+        downloadFile(item.url, item.name)
+    }
+
+    private fun downloadFile(URl: String?, imageName : String?) {
+        val direct = File(
+            Environment.getExternalStorageDirectory()
+                .toString()
+        )
+        if (!direct.exists()) {
+            direct.mkdirs()
+        }
+        val mgr = requireActivity().getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+        val downloadUri = Uri.parse(URl)
+        val request = DownloadManager.Request(
+            downloadUri
+        )
+        request.setAllowedNetworkTypes(
+            DownloadManager.Request.NETWORK_WIFI
+                    or DownloadManager.Request.NETWORK_MOBILE
+        )
+            .setAllowedOverRoaming(false).setTitle("Demo")
+            .setDescription("Something useful. No, really.")
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "${imageName}.jpeg")
+        mgr.enqueue(request)
+
+        snackbar = Snackbar.make(
+            requireActivity().findViewById(R.id.content),
+            "Download Completed", Snackbar.LENGTH_LONG
+        ).setAction("Downloads", View.OnClickListener {
+            startActivity(Intent(DownloadManager.ACTION_VIEW_DOWNLOADS))
+        }
+
+        )
+        snackbar.show()
+    }
+
 
 }
